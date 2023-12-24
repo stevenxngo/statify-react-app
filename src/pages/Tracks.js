1110a551be9b67f467afe9109b694d9b5be96094
Steven Ngo <stevenxngo@login-students.khoury.northeastern.edu>
2023-12-23 22:12:06 -0500

    Add basic display for tracks

--- src/pages/Tracks.js
+++ src/pages/Tracks.js
@@ -1,23 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
-import { Button } from "react-bootstrap";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
 import TimeNav from "../components/TimeNav";
 import { getTop } from "../services/userServices";
+import "../styles/tracks.css";
 
 function Tracks() {
   const { timespan } = useParams();
+  const [fetchingData, setFetchingData] = useState(true);
   const [tracks, setTracks] = useState([]);
 
-  const getTracks = async () => {
-    const response = await getTop("tracks", timespan);
-    console.log(response);
-  }
+  useEffect(() => {
+    const fetchTracks = async () => {
+      setFetchingData(true);
+      const response = await getTop("tracks", timespan);
+      let tracks = response.items;
+      tracks = tracks.map((track) => ({
+        id: track.id,
+        name: track.name,
+        images: track.album.images,
+        artists: track.artists.map((artist) => ({
+          id: artist.id,
+          name: artist.name,
+        })),
+      }));
+      console.log(tracks);
+      setTracks(tracks);
+      setFetchingData(false);
+    };
+    fetchTracks();
+  }, [timespan]);
 
   return (
     <div>
-      <TimeNav type={ "tracks" } />
+      <TimeNav type={"tracks"} />
       <h1>Tracks</h1>
-      <Button onClick={getTracks}>Get Tracks</Button>
+      {fetchingData ? (
+        <p>Fetching data...</p>
+      ) : (
+        <ListGroup as="ol" numbered className="tracks-list">
+          {tracks.map((track) => (
+            <a
+              href={`https://open.spotify.com/track/${track.id}/`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="track-link"
+            >
+              <ListGroupItem key={track.id} as="li">
+                {track.name}
+              </ListGroupItem>
+            </a>
+          ))}
+        </ListGroup>
+      )}
     </div>
   );
 }
