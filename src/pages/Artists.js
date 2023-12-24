3fc43a4a440b2c8347e7d88c3e4543a434872bc8
Steven Ngo <stevenxngo@login-students.khoury.northeastern.edu>
2023-12-23 23:30:41 -0500

    Implement get artists and basic display

--- src/pages/Artists.js
+++ src/pages/Artists.js
@@ -1,22 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
-import { Button } from "react-bootstrap";
 import TimeNav from "../components/TimeNav";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
 import { getTop } from "../services/userServices";
 
 function Artists() {
   const { timespan } = useParams();
+  const [fetchingData, setFetchingData] = useState(true);
+  const [artists, setArtists] = useState([]);
 
-  const getArtists = async () => {
-    const response = await getTop("artists", timespan);
-    console.log(response);
-  }
+  useEffect(() => {
+    const fetchArtists = async () => {
+      setFetchingData(true);
+      const response = await getTop("artists", timespan);
+      let artists = response.items;
+      artists = artists.map((artist) => ({
+        id: artist.id,
+        name: artist.name,
+        images: artist.images,
+        genres: artist.genres,
+        popularity: artist.popularity,
+      }));
+      console.log(artists);
+      setArtists(artists);
+      setFetchingData(false);
+    };
+    fetchArtists();
+  }, [timespan]);
 
   return (
     <div>
-      <TimeNav type={ "artists" } />
+      <TimeNav type={"artists"} />
       <h1>Artists</h1>
-      <Button onClick={getArtists}>Get Artists</Button>
+      {fetchingData ? (
+        <p>Fetching data...</p>
+      ) : (
+        <ListGroup as="ol" numbered className="tracks-list">
+          {artists.map((artist) => (
+            <a
+              href={`https://open.spotify.com/artist/${artist.id}/`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="track-link"
+            >
+              <ListGroupItem key={artist.id} as="li">
+                {artist.name}
+              </ListGroupItem>
+            </a>
+          ))}
+        </ListGroup>
+      )}
     </div>
   );
 }
