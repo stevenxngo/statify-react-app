a0a73d629b2022b41c01a060349310f26fb9dbfd
Steven Ngo <stevenxngo@login-students.khoury.northeastern.edu>
2023-12-26 01:04:37 -0500

    Finished artists page

--- src/pages/Artists.js
+++ src/pages/Artists.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import TimeNav from "../components/TimeNav";
-import { ListGroup, ListGroupItem } from "react-bootstrap";
+import { Container, ListGroup, ListGroupItem, Row, Col } from "react-bootstrap";
+import { FaSpotify } from "react-icons/fa";
+import Spinner from "../components/Spinner";
 import { getTop } from "../services/userServices";
+import "../styles/tracks.css";
 
 function Artists() {
   const { timespan } = useParams();
@@ -32,25 +35,77 @@ function Artists() {
 
   return (
     <div>
-      <h1>artists</h1>
+      <Container className="d-flex justify-content-center">
+        <h1 className="tracks-page-title">artists</h1>
+      </Container>
       <TimeNav type={"artists"} />
       {fetchingData ? (
-        <p>fetching data...</p>
+        <Spinner />
       ) : (
-        <ListGroup as="ol" numbered className="tracks-list">
-          {artists.map((artist) => (
-            <a
-              href={`https://open.spotify.com/artist/${artist.id}/`}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="track-link"
+        <div className="mt-3">
+          <Row className="justify-content-center">
+            {artists.slice(0, 3).map((artist, index) => (
+              <Col key={artist.id} xs={4} sm={3} className="text-center mb-3">
+                <div className="square-image-container">
+                  <img
+                    src={artist.images[0].url}
+                    alt={artist.name}
+                    className="img-fluid square-image"
+                  />
+                </div>
+                <ListGroup className="">
+                  <ListGroupItem className="rounded-0 track-item">{`${
+                    index + 1
+                  }. ${artist.name}`}</ListGroupItem>
+                </ListGroup>
+              </Col>
+            ))}
+          </Row>
+
+          {artists.slice(3).map((artist, index) => (
+            <Row
+              key={artist.id}
+              className="align-items-center justify-content-center mb-1"
+              noGutters
             >
-              <ListGroupItem key={artist.id} as="li">
-                {artist.name}
-              </ListGroupItem>
-            </a>
+              <Col xs={1} className="text-center px-0 ">
+                <p className="track-rank align-items-center">{index + 4}</p>
+              </Col>
+              <Col xs="auto" className="text-center px-0">
+                <div className="">
+                  <img
+                    src={artist.images[0].url}
+                    alt={artist.name}
+                    className="img-fluid other-track-img px-0"
+                    style={{
+                      width: "72px",
+                      objectFit: "cover",
+                      height: "100%",
+                    }}
+                  />
+                </div>
+              </Col>
+              <Col xs={8} className="px-0">
+                <ListGroup className="mb-0">
+                  <ListGroupItem className="rounded-0 track-item pb-0 ">{`${artist.name}`}</ListGroupItem>
+                  <ListGroupItem className="rounded-0 track-item">
+                    {artist.genres.map((genre) => genre).join(", ")}
+                  </ListGroupItem>
+                </ListGroup>
+              </Col>
+              <Col xs={1} className="text-center px-0 ">
+                <a
+                  href={`https://open.spotify.com/artist/${artist.id}/`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="white"
+                >
+                  <FaSpotify className="spotify-icon" />
+                </a>
+              </Col>
+            </Row>
           ))}
-        </ListGroup>
+        </div>
       )}
     </div>
   );
