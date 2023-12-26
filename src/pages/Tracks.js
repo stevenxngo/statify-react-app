ed4fe681b4784795896d37b0c019170c10311041
Steven Ngo <stevenxngo@login-students.khoury.northeastern.edu>
2023-12-26 00:49:10 -0500

    Finished tracks page

--- src/pages/Tracks.js
+++ src/pages/Tracks.js
@@ -4,6 +4,7 @@ import { ListGroup, ListGroupItem, Container, Row, Col } from "react-bootstrap";
 import TimeNav from "../components/TimeNav";
 import { getTop } from "../services/userServices";
 import Spinner from "../components/Spinner";
+import { FaSpotify } from "react-icons/fa";
 import "../styles/tracks.css";
 
 function Tracks() {
@@ -43,20 +44,20 @@ function Tracks() {
       {fetchingData ? (
         <Spinner />
       ) : (
-        <div>
-          <Row>
+        <div className="mt-3">
+          <Row className="justify-content-center">
             {tracks.slice(0, 3).map((track, index) => (
-              <Col key={track.id} md={4} className="text-center mb-3">
+              <Col key={track.id} xs={4} sm={3} className="text-center mb-3">
                 <img
                   src={track.images[0].url}
                   alt={track.name}
                   className="img-fluid"
                 />
-                <ListGroup className="mt-2">
-                  <ListGroupItem as="h5">{`#${index + 1} ${
-                    track.name
-                  }`}</ListGroupItem>
-                  <ListGroupItem>
+                <ListGroup className="">
+                  <ListGroupItem className="rounded-0 track-item pb-0">{`${
+                    index + 1
+                  }. ${track.name}`}</ListGroupItem>
+                  <ListGroupItem className="rounded-0 track-item">
                     {track.artists.map((artist) => artist.name).join(", ")}
                   </ListGroupItem>
                 </ListGroup>
@@ -65,24 +66,39 @@ function Tracks() {
           </Row>
 
           {tracks.slice(3).map((track, index) => (
-            <Row key={track.id} className="mb-3">
-              <Col md={1} className="text-center">
-                {index + 4}
+            <Row
+              key={track.id}
+              className="align-items-center justify-content-center mb-1"
+              noGutters
+            >
+              <Col xs={1} className="text-center px-0 ">
+                <p className="track-rank align-items-center">{index + 4}</p>
               </Col>
-              <Col md={5}>
+              <Col xs="auto" className="text-center px-0">
+                <img
+                  src={track.images[0].url}
+                  alt={track.name}
+                  className="img-fluid other-track-img px-0"
+                  style={{ height: "100%" }}
+                />
+              </Col>
+              <Col xs={8} className="px-0">
+                <ListGroup className="mb-0">
+                  <ListGroupItem className="rounded-0 track-item pb-0 ">{`${track.name}`}</ListGroupItem>
+                  <ListGroupItem className="rounded-0 track-item">
+                    {track.artists.map((artist) => artist.name).join(", ")}
+                  </ListGroupItem>
+                </ListGroup>
+              </Col>
+              <Col xs={1} className="text-center px-0 ">
                 <a
                   href={`https://open.spotify.com/track/${track.id}/`}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="track-link"
+                  className="white"
                 >
-                  <ListGroupItem as="h5">{track.name}</ListGroupItem>
+                  <FaSpotify className="spotify-icon" />
                 </a>
-              </Col>
-              <Col md={6}>
-                <ListGroupItem>
-                  {track.artists.map((artist) => artist.name).join(", ")}
-                </ListGroupItem>
               </Col>
             </Row>
           ))}
