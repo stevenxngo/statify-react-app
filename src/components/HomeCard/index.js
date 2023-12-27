32289e4ad2503bed6fec24db6fee3c43a7768810
Steven Ngo <stevenxngo@login-students.khoury.northeastern.edu>
2023-12-27 18:38:14 -0500

    Start support for genres

--- src/components/HomeCard/index.js
+++ src/components/HomeCard/index.js
@@ -11,13 +11,15 @@ function HomeCard({ loggedIn }) {
         src={homecardimg}
         alt="Spotify Genres"
         className="homecard-img"
+        loading="lazy"
       />
       <Card.ImgOverlay className="d-flex flex-column align-items-center justify-content-around">
         <Card.Title>statify</Card.Title>
         {loggedIn ? (
             <div className="mb-2 homecard-btns">
-              <HomeCardButton variant="tracks" className="mb-3"/>
-              <HomeCardButton variant="artists" />
+              <HomeCardButton variant="tracks" className="mb-2"/>
+              <HomeCardButton variant="artists" className="mb-2"/>
+              <HomeCardButton variant="genres" />
             </div>
           ) : (
             <div className="mb-2 d-flex flex-column homecard-btns align-items-center justify-content-center">
